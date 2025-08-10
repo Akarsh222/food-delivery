b31371078346c4d7928a4e5ea7a283b88be4b059
Akarsh <Akarsh924sri@mgail.com>
2025-08-10 17:28:07 +0530

    Navbar Updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,27 @@
-import React, { useState } from "react";
+import React, { act, useEffect, useState } from "react";
 import { assets } from "../../assets/assets.js";
 import "./Navbar.css";
 const Navbar = () => {
   const[active, setActive] = useState("home");
+
+  
   return (
     <div className="main-navbar">
       <img src={assets.logo} alt="logo-icon" />
       <ul className="menu-icons">
-        <li className="active">Home</li>
-        <li>Menu</li>
-        <li>Contact Us</li>
-        <li>About Us</li>
-        <li>Mobile App</li>
+        <li onClick={() => setActive( (prev) => prev = "home")} className={active == "home" ? "active" : ""}>Home</li>
+        <li onClick={() => setActive( (prev) => prev = "menu")} className={active == "menu" ? "active" : ""}> Menu</li>
+        <li onClick={() => setActive( (prev) => prev = "contact")} className={active == "contact" ? "active" : ""}>Contact Us</li>
+        <li onClick={() => setActive( (prev) => prev = "about")} className={active == "about" ? "active" : ""}>About Us</li>
+        <li onClick={() => setActive( (prev) => prev = "mobile")} className={active == "mobile" ? "active" : ""}>Mobile App</li>
       </ul>
       <div className="right-navbar">
-        <img src={assets.search_icon} alt="" />
+        <img src={assets.search_icon} alt="" className="search"/>
         <div className="cart">
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>Sign Up</button>
+        <button className="text-sm">Sign Up</button>
       </div>
     </div>
   );
